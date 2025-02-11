8e46d8b003e72957240961e21d721a1694154521
Maxwell Moura <maxwellcmoura@gmail.com>
2025-02-11 16:57:45 -0300

    subindo o primeiro projeto da barbearia

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Codigo pro Menu
+function clickMenu() {
+   if(menuItens.style.display == 'block') {
+       menuItens.style.display = 'none'
+   }else{
+       menuItens.style.display = 'block'
+   }
+}
+function redirecionar() {
+    window.location.href = "agenda.html";
+  }
